b1068112a35823d30db6d9142281120d4face1a8
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2020-12-29 09:48:37 +0000

    Tiku

--- serveris.py
+++ serveris.py
@@ -10,6 +10,15 @@ def index():
 @app.route('/sveiki')
 def sveiki():
     return "Vairs nav rīts!"
+@app.route('/sveiki/<vards>')
+def sveikivards(vards):
+    return f"Sveiki, {vards}"
+@app.route('/cik/<sk1>/<sk2>')
+def reizinajums(sk1,sk2):
+    sk1 = int(sk1)
+    sk2 = int(sk2)
+    reizinajums = sk1*sk2
+    return str(reizinajums)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
