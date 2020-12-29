5ba975e73e7ec2193078ea43494dfcdee3f080b6
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2020-12-29 09:21:59 +0000

    sveiki

--- serveris.py
+++ serveris.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Labrīt!"
-
+@app.route('/sveiki')
+def sveiki():
+    return "Vairs nav rīts!"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
